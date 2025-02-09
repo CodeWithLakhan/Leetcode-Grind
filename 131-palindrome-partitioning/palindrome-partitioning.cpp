ecc01c98f289d91228eb07e9a7180fbb412e1307
Lakhan Agrawal <121571107+CodeWithLakhan@users.noreply.github.com>
2025-02-09 19:29:58 +0530

    Time: 36 ms (69.96%) | Memory: 54.6 MB (66.77%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> parts;
+
+        solve( s , parts , ans);
+        return ans;
+    }
+    
+    void solve (string s , vector<string> &parts , vector<vector<string>> &ans){
+        if(s.size()==0)
+        {
+            ans.push_back(parts);
+            return;
+        }
+        for(int i=0;i<s.size() ; i++)
+        {
+            string temp = s.substr( 0 , i+1);
+            if(ispalin(temp))
+            {
+                parts.push_back(temp);
+                solve( s.substr(i+1 , s.size()) , parts , ans );
+                parts.pop_back();
+            }
+        }
+    }
+    bool ispalin(string s){
+            string x = s;
+            reverse(x.begin() , x.end());
+            return x==s;
+        }
+};
